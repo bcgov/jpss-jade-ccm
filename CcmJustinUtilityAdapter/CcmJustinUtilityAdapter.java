6f16ffcaae7b28b1d412975b2f5867249f019614
David Agahchen <david@agahchen.ca>
2022-06-13 13:08:29 -0700

    BCPSDEMS-112 updates

--- CcmJustinUtilityAdapter/CcmJustinUtilityAdapter.java
+++ CcmJustinUtilityAdapter/CcmJustinUtilityAdapter.java
@@ -6,10 +6,42 @@
 //
 
 // camel-k: language=java
-// camel-k: dependency=mvn:org.apache.camel.quarkus:camel-quarkus-kafka
+// camel-k: dependency=mvn:org.apache.camel.quarkus:camel-quarkus-kafka:camel-quarkus-jsonpath
 
-//import org.apache.camel.Exchange;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+
+class EventsProcessor implements Processor {
+
+  public EventsProcessor() {
+  }
+
+  public void process(Exchange exchange) throws Exception {
+    // Insert code that gets executed *before* delegating
+    // to the next processor in the chain.
+
+    String body = exchange.getIn().getBody(String.class);
+
+    System.out.println("Received message: " + body);
+
+    if (body != null && body.contains("Kaboom")) {
+      throw new Exception("Illegal data found!");
+    }
+
+    // Unmarshalling a JSON Array Using camel-jackson
+    // https://www.baeldung.com/java-camel-jackson-json-array
+
+    // Intro to the Jackson ObjectMapper
+    // https://www.baeldung.com/jackson-object-mapper-tutorial
+
+    // Quarkus Jsonpath
+    // https://camel.apache.org/camel-quarkus/2.9.x/reference/extensions/jsonpath.html
+    // https://camel.apache.org/components/3.16.x/languages/jsonpath-language.html)
+
+    exchange.getMessage().setBody("OK");
+  }
+}
 
 public class CcmJustinUtilityAdapter extends RouteBuilder {
   @Override
@@ -26,7 +58,23 @@ public class CcmJustinUtilityAdapter extends RouteBuilder {
     //
     // BCPSDEMS-143 Kafka routing below commented out until deployment issue is resolved
     .to("kafka:{{kafka.topic.name}}");
-    ;
+
+    from("platform-http:/v1/health?httpMethodRestrict=GET")
+    .routeId("healthCheck")
+    .removeHeaders("CamelHttp*")
+    .log("/v1/health request received")
+    .setHeader(Exchange.HTTP_METHOD, simple("GET"))
+    .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+    .to("https://dev.jag.gov.bc.ca/ords/devj/justinords/dems/v1/health");
+
+    from("timer://simpleTimer?period={{notification.check.frequency}}")
+    .routeId("getNotifications")
+    .log("checking for new notificatoins...")
+    .setHeader(Exchange.HTTP_METHOD, simple("GET"))
+    .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+    .to("https://dev.jag.gov.bc.ca/ords/devj/justinords/dems/v1/inProgressEvents")
+    .process(new EventsProcessor())
+    .log("In progress events from JUSTIN: ${body}");
   }
 }
 
