96d3dfab0f11401f5e3f9d41752fb698f8d4f5d7
David Agahchen <david@agahchen.ca>
2022-08-23 12:44:01 -0700

    Aug 23 updates

--- src/main/java/ccm/CcmNotificationService.java
+++ src/main/java/ccm/CcmNotificationService.java
@@ -108,11 +108,11 @@ public class CcmNotificationService extends RouteBuilder {
     .setHeader(Exchange.HTTP_METHOD, simple("POST"))
     .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
     .setHeader("number").simple("${header.event_object_id}")
-    .to("http://ccm-lookup-service/getCourtCaseDetails")
+    ////.to("http://ccm-lookup-service/getCourtCaseDetails")
     .log("Create court case in DEMS.  body = ${body}.")
-    .to("http://ccm-dems-adapter/createCourtCase")
-    .log("Process court case auth list changed")
-    .to("direct:processCourtCaseAuthListChanged")
+    ////.to("http://ccm-dems-adapter/createCourtCase")
+    .log("Update court case auth list.")
+    ////.to("direct:processCourtCaseAuthListChanged")
     ;
 
     from("direct:processCourtCaseAuthListChanged")
