d52ae1f4c3b526413c90f8be7575e83c3a6be963
David Agahchen <david@agahchen.ca>
2023-04-17 13:34:49 -0700

    Enabled bearer token verification

--- src/main/java/ccm/CcmJustinInAdapter.java
+++ src/main/java/ccm/CcmJustinInAdapter.java
@@ -27,7 +27,6 @@ public class CcmJustinInAdapter extends RouteBuilder {
   public void configure() throws Exception {
     version();
     getCaseHyperlink();
-    getCaseHyperlinkNew();
   }
 
   private void version() {
@@ -49,112 +48,6 @@ public class CcmJustinInAdapter extends RouteBuilder {
   }
 
   private void getCaseHyperlink() {
-    // use method name as route id
-    String routeId = new Object() {}.getClass().getEnclosingMethod().getName();
-
-    String path = "justin/api/v1/" + routeId;
-
-    // IN: header = rcc_id
-    from("platform-http:/" + path + "?httpMethodRestrict=GET")
-    .routeId(routeId)
-    .streamCaching() // https://camel.apache.org/manual/faq/why-is-my-message-body-empty.html
-
-    .process(new Processor() {
-      @Override
-      public void process(Exchange exchange) throws Exception {
-        exchange.setProperty("exchangeId",exchange.getExchangeId());
-      }
-    })
-    .log(LoggingLevel.INFO, "Received request (exchange id: ${exchangeProperty.exchangeId}) for case hyperlink. RCC_ID: ${header.rcc_id}")
-
-/*     .choice()
-      .when(simple("${header.authorization} != 'Bearer {{justin.in.token}}'"))
-        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(401))
-        .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-        .process(new Processor() {
-          @Override
-          public void process(Exchange exchange) throws Exception {
-            JustinCaseHyperlinkData body = new JustinCaseHyperlinkData();
-            body.setMessage("Unauthorized.");
-            exchange.getMessage().setBody(body);
-          }
-        })
-        .marshal().json(JsonLibrary.Jackson, JustinCaseHyperlinkData.class)
-        .log(LoggingLevel.DEBUG,"HTTP response 401. Body: ${body}")
-        .stop()
-      .end() */
-
-    .setProperty("rcc_id", simple("${header.rcc_id}"))
-
-    .choice()
-      .when(PredicateBuilder.or(
-          header("rcc_id").isNull(),
-          header("rcc_id").isEqualTo("")))
-        .log(LoggingLevel.INFO, "RCC_ID is empty.")
-        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(400))
-        .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-
-        .process(new Processor() {
-          @Override
-          public void process(Exchange exchange) throws Exception {
-            JustinCaseHyperlinkData body = new JustinCaseHyperlinkData();
-            body.setMessage("Required parameter (RCC_ID) is missing.");
-            exchange.getMessage().setBody(body);
-          }
-        })
-        .marshal().json(JsonLibrary.Jackson, JustinCaseHyperlinkData.class)
-        .stop()
-        .endChoice()
-    .end()
-
-    .removeHeader("CamelHttpUri")
-    .removeHeader("CamelHttpBaseUri")
-    .removeHeaders("CamelHttp*")
-    .setHeader(Exchange.HTTP_METHOD, simple("GET"))
-
-    // attempt to retrieve case id using getCourtCaseExists lookup endpoint.
-    .setHeader("number", simple("${exchangeProperty.rcc_id}"))
-    .to("http://ccm-lookup-service/getCourtCaseExists")
-    .unmarshal().json()
-    .setProperty("caseId").simple("${body[id]}")
-    .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-    .choice()
-      .when(simple("${exchangeProperty.caseId} != ''"))
-        .setProperty("hyperlinkPrefix", simple("{{dems.case.hyperlink.prefix}}"))
-        .setProperty("hyperlinkSuffix", simple("{{dems.case.hyperlink.suffix}}"))
-        .process(new Processor() {
-          @Override
-          public void process(Exchange exchange) throws Exception {
-            String prefix = exchange.getProperty("hyperlinkPrefix", String.class);
-            String suffix = exchange.getProperty("hyperlinkSuffix", String.class);
-            String caseId = exchange.getProperty("caseId", String.class);
-            JustinCaseHyperlinkData body = new JustinCaseHyperlinkData();
-
-            body.setMessage("Case found.");
-            body.setHyperlink(prefix + caseId + suffix);
-            exchange.getMessage().setBody(body);
-          }
-        })
-        .log(LoggingLevel.INFO, "Case (RCC_ID: ${exchangeProperty.rcc_id}) found; caseId: '${exchangeProperty.caseId}'")
-        .endChoice()
-      .otherwise()
-        .process(new Processor() {
-          @Override
-          public void process(Exchange exchange) throws Exception {
-            JustinCaseHyperlinkData body = new JustinCaseHyperlinkData();
-            body.setMessage("Case not found.");
-            exchange.getMessage().setBody(body);
-          }
-        })
-        .log(LoggingLevel.INFO, "Case not found.")
-        .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(404))
-        .endChoice()
-    .end()
-    .marshal().json(JsonLibrary.Jackson, JustinCaseHyperlinkData.class)
-    ;
-  }
-
-  private void getCaseHyperlinkNew() {
     // use method name as route id
     String routeId = new Object() {}.getClass().getEnclosingMethod().getName();
 
