5482c7ad4adf03ca8a9e8d028aba8b224ec7dc18
elainecwong <100694125+elainecwong@users.noreply.github.com>
2023-05-04 11:04:57 -0700

    Code verification and minor clean-up and fixes.

--- src/main/java/ccm/CcmNotificationService.java
+++ src/main/java/ccm/CcmNotificationService.java
@@ -265,6 +265,8 @@ public class CcmNotificationService extends RouteBuilder {
       .jsonpath("$.event_key")
     .setHeader("event_status")
       .jsonpath("$.event_status")
+    .setHeader("event_message_id")
+      .jsonpath("$.justin_event_message_id")
     .setHeader("event")
       .simple("${body}")
     .unmarshal().json(JsonLibrary.Jackson, ChargeAssessmentEvent.class)
@@ -348,15 +350,13 @@ public class CcmNotificationService extends RouteBuilder {
     .to("http://ccm-dems-adapter/createCourtCase")
     .log(LoggingLevel.DEBUG,"Update court case auth list.")
     .to("direct:processCourtCaseAuthListChanged")
-    .log(LoggingLevel.INFO, "Create ReportEvent for Information report")
-    // create Report Event for an INFORMATION type report.
-    .setBody(simple("${exchangeProperty.metadata_data}"))
-    .unmarshal().json(JsonLibrary.Jackson, CourtCaseData.class)
+    .log(LoggingLevel.INFO, "Create ReportEvent for Static reports")
+    // create Report Event for static type reports.
     .process(new Processor() {
       @Override
       public void process(Exchange exchange) {
-        CourtCaseData bcm = exchange.getIn().getBody(CourtCaseData.class);
         String event_message_id = exchange.getMessage().getHeader("event_message_id", String.class);
+        String rcc_id = exchange.getMessage().getHeader("event_key", String.class);
         StringBuilder reportTypesSb = new StringBuilder("");
         reportTypesSb.append(ReportEvent.REPORT_TYPES.NARRATIVE.name() + ",");
         reportTypesSb.append(ReportEvent.REPORT_TYPES.SYNOPSIS.name() + ",");
@@ -367,13 +367,11 @@ public class CcmNotificationService extends RouteBuilder {
         reportTypesSb.append(ReportEvent.REPORT_TYPES.VEHICLE.name());
 
         ReportEvent re = new ReportEvent();
-        re.setRcc_ids("${header.event_key}");
+        re.setJustin_rcc_id(rcc_id);
         re.setEvent_status(ReportEvent.STATUS.REPORT.name());
-        //re.setEvent_key(bcm.getCourt_file_id());
         re.setEvent_source(ReportEvent.SOURCE.JADE_CCM.name());
         re.setJustin_event_message_id(Integer.parseInt(event_message_id));
         re.setJustin_message_event_type_cd(ReportEvent.STATUS.REPORT.name());
-        //re.setMdoc_justin_no(bcm.getCourt_file_id());
         re.setReport_type(reportTypesSb.toString());
         exchange.getMessage().setBody(re, ReportEvent.class);
       }
@@ -381,7 +379,6 @@ public class CcmNotificationService extends RouteBuilder {
     .marshal().json(JsonLibrary.Jackson, ReportEvent.class)
     .to("kafka:{{kafka.topic.reports.name}}")
     ;
-    
   }
 
   private void processCourtCaseEvents() {
