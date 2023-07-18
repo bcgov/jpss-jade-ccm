99785ab32383a5b2d71612bbf149b2f9aee7c9da
swezamm <skutty@salussystems.com>
2023-07-18 15:32:11 -0700

    fix for jade 2621

--- src/main/java/ccm/models/system/dems/DemsFieldData.java
+++ src/main/java/ccm/models/system/dems/DemsFieldData.java
@@ -90,7 +90,10 @@ public class DemsFieldData {
         LAST_JUSTIN_UPDATE("Last JUSTIN Update"),
         LAST_API_RECORD_UPDATE("Last API Record Update"),
         //added as part of jade-2483
-        CASE_STATE("Case State");
+        CASE_STATE("Case State"),
+        //added as part of jade-2621
+        PRIMARY_AGENCY_FILE_ID("Primary Agency File ID"),
+        PRIMARY_AGENCY_FILE_NO("Primary Agency File No");
 
         private String label;
 
