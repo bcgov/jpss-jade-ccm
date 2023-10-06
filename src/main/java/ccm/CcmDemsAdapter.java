df5e1c9af93f82289991613b4c94f6f690597b17
swezamm <skutty@salussystems.com>
2023-10-06 13:50:15 -0700

    jade 1750

--- src/main/java/ccm/CcmDemsAdapter.java
+++ src/main/java/ccm/CcmDemsAdapter.java
@@ -2906,7 +2906,8 @@ public class CcmDemsAdapter extends RouteBuilder {
     from("platform-http:/" + routeId)
     .routeId(routeId)
     .streamCaching() // https://camel.apache.org/manual/faq/why-is-my-message-body-empty.html
-    .log(LoggingLevel.INFO,"fromPartid = ${header[fromPartid]} toPartid = ${header[toPartid]}")
+    .log(LoggingLevel.INFO,"fromPartid = ${header[fromPartid]} ")
+    .log(LoggingLevel.INFO,"toPartid = ${header[toPartid]}")
 
     .removeHeader("CamelHttpUri")
     .removeHeader("CamelHttpBaseUri")
@@ -2914,7 +2915,7 @@ public class CcmDemsAdapter extends RouteBuilder {
     .setHeader(Exchange.HTTP_METHOD, simple("POST"))
     .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
     .setHeader("Authorization").simple("Bearer " + "{{dems.token}}")
-    .toD("https://{{dems.host}}/org-units/{{dems.org-unit.id}}/persons/${header.fromPartid}/reassign- cases/${header.toPartid}")
+    .toD("https://{{dems.host}}/org-units/{{dems.org-unit.id}}/persons/${header.fromPartid}/reassign-cases/${header.toPartid}")
     .log(LoggingLevel.INFO, "response: '${body}'")
       
     .end()
