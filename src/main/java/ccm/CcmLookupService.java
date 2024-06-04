815e9e097026b6349a2667f4ab120308ce8f47e1
swezamm <skutty@salussystems.com>
2024-06-04 13:02:53 -0700

    fix for jade 2906: Disposition date for court file id

--- src/main/java/ccm/CcmLookupService.java
+++ src/main/java/ccm/CcmLookupService.java
@@ -59,6 +59,7 @@ public class CcmLookupService extends RouteBuilder {
     getCaseHyperlink();
     getCaseListHyperlink();
     getJustinFileClose();
+    getFileDisp();
   }
 
 
@@ -639,4 +640,22 @@ public class CcmLookupService extends RouteBuilder {
    .log(LoggingLevel.DEBUG,"response from JUSTIN: ${body}")
    ;
  }
+
+ private void getFileDisp() {
+  // use method name as route id
+  String routeId = new Object() {}.getClass().getEnclosingMethod().getName();
+
+  from("platform-http:/" + routeId)
+  .routeId(routeId)
+  .streamCaching() // https://camel.apache.org/manual/faq/why-is-my-message-body-empty.html
+  .removeHeader("CamelHttpUri")
+  .removeHeader("CamelHttpBaseUri")
+  .removeHeaders("CamelHttp*")
+  .log(LoggingLevel.DEBUG,"Processing request... number = ${header[number]}")
+  .setHeader(Exchange.HTTP_METHOD, simple("GET"))
+  .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+  .to("http://ccm-justin-out-adapter/getFileDisp")
+  .log(LoggingLevel.DEBUG,"response from JUSTIN: ${body}")
+  ;
+}
 }
