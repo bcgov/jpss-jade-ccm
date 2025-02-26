ec9febd1bdc31e36719d429aad645e267e839342
Todd Wuolle <twuolle@parcsystems.ca>
2025-02-26 10:43:22 -0800

    Added call to justin out

--- src/main/java/ccm/CcmLookupService.java
+++ src/main/java/ccm/CcmLookupService.java
@@ -64,10 +64,14 @@ public class CcmLookupService extends RouteBuilder {
     getFileDisp();
     getFileNote();
     getFileCloseData();
-  }
-
-
-  private void attachExceptionHandlers() {
+    getAgencyFileStatus();
+      }
+    
+    
+      
+    
+    
+      private void attachExceptionHandlers() {
 
     // handle network connectivity errors
     onException(ConnectException.class, SocketTimeoutException.class, HttpHostConnectException.class)
@@ -776,6 +780,24 @@ public class CcmLookupService extends RouteBuilder {
     .to("http://ccm-justin-out-adapter/getFileNote")
     .log(LoggingLevel.INFO,"response from JUSTIN: ${body}")
     ;
+  }
+
+  private void getAgencyFileStatus() {
+    // use method name as route id
+    String routeId = new Object() {}.getClass().getEnclosingMethod().getName();
+    
+    from("platform-http:/" + routeId)
+  
+    .routeId(routeId)
+    .streamCaching() // https://camel.apache.org/manual/faq/why-is-my-message-body-empty.html
+    .removeHeader("CamelHttpUri")
+    .removeHeader("CamelHttpBaseUri")
+    .removeHeaders("CamelHttp*")
+    .log(LoggingLevel.INFO,"Processing request... agencyIdCode = ${header.agencyIdCode} agnecyFileNumber= ${header.agencyFileNumber}}")
+    .setHeader(Exchange.HTTP_METHOD, simple("GET"))
+    .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+    .to("http://ccm-justin-out-adapter/getAgencyFileStatus?throwExceptionOnFailure=false")
+    .log(LoggingLevel.INFO,"response from JUSTIN: ${body}");
   }
  
 }
